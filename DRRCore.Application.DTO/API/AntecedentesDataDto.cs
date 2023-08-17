8ac0b10746aff2c89a848ac3cabdaeb2ea0efffb
Usuario <Usuario@LAPTOP-2JMIOR9E>
2023-08-17 09:59:23 -0500

    fdsf

---
+++ DRRCore.Application.DTO/API/AntecedentesDataDto.cs
@@ -0,0 +1,53 @@
+﻿namespace DRRCore.Application.DTO.API
+{
+    public class AntecedentesDataDto
+    {
+        public string? CodigoInforme { get; set; }
+        public string? FechaConstitucion { get; set; }
+        public string? InicioActividades { get; set; }
+        public string? Duracion { get; set; }
+        public string? RegistradaEn { get; set; }
+        public string? Notaria { get; set; }
+        public string? RegistrosPublicos { get; set; }
+        public CapitalPagadoDto? CapitalPagadoActual { get; set; }
+        public string? Origen { get; set; }
+        public AumentoDto? FechaAumento { get; set; }
+        public string? CotizaEnBolsa { get; set; }
+        public string? AumentoPor { get; set; }
+        public TipoCambioDto? ActualTipoCambio { get; set; }
+        public RRPPDto? ConsultaRRPP { get; set; }
+        public string? Antecedentes { get; set; }
+        public string? Comentarios { get; set; }
+        public string? Flag { get; set; } // 1-0
+      
+        public string? DescripcionGeneral { get; set; }
+
+
+    }
+    public class CapitalPagadoDto
+    {
+        public int Cantidad { get; set; }
+        public string? Moneda { get; set; }
+        public string? Descripcion { get; set; }
+        public string? Comentario { get; set; }
+    }
+    public class AumentoDto
+    {
+        public string? Fecha { get; set; }
+        public string? Descripcion { get; set; }
+    }
+    public class TipoCambioDto
+    {
+        public decimal? Monto { get; set; }
+        public string? Moneda { get; set; }
+        public string? Descripcion { get; set; }
+        public string? Observacion { get; set; }
+    }
+    public class RRPPDto
+    {
+        public string? Fecha { get; set; }
+        public string? Descripcion { get; set; }
+    }
+    
+}
+
---
+++ DRRCore.Application.DTO/API/RamoNegocioDataDto.cs
@@ -0,0 +1,56 @@
+﻿namespace DRRCore.Application.DTO.API
+{
+    public class RamoNegocioDataDto
+    {
+        public string? CodigoInforme { get; set; }
+        public int? CodigoCupon { get; set; }
+        public int? SectorCodigo { get; set; }
+        public string? SectorCodigo_EE { get; set; }
+        public int? RamoCodigo { get; set; }
+        public string? RamoCodigo_EE { get; set; }
+        public ImportacionDto? Importa { get; set; }
+        public ExportacionDto? Exporta { get; set; }
+        public VentaContadoDto? VentaContado { get; set; }
+        public VentaCreditoDto? VentaCredito { get; set; }
+        public string? TerritorioVentas { get; set; }
+        public string? VentaExterior { get; set; }
+        public string? CompraNacional { get; set; }
+        public string? CompraExtranjero { get; set; }
+        public int? CantidadTrabajadores { get; set; }
+        public string? TitularidadLocal { get; set; }
+        public string? ObservacionLocal { get; set; }
+        public string? AreaLocal { get; set; }
+        public string? PlantasLocal { get; set; }
+        public string? AlmacenesLocal { get; set; }
+        public string? DomicilioAnterior { get; set; }
+        public string? Actividades { get; set; }
+        public string? Comentarios { get; set; }
+        public string? Tabulado { get; set; }
+        public int? Flag { get; set; }
+       
+        public string? DescripcionGeneral { get; set; } 
+
+    }
+    public class ImportacionDto
+    {
+        public string? Importan { get; set; }
+        public string? Paises { get; set; }
+    }
+    public class ExportacionDto
+    {
+        public string? Exportan { get; set; }
+        public string? Paises { get; set; }
+    }
+    public class VentaContadoDto
+    {
+        public int? Cantidad { get; set; }
+        public string? Descripcion { get; set; }
+        public string? Observacion { get;}
+    }
+    public class VentaCreditoDto
+    {
+        public int? Cantidad { get; set; }
+        public string? Descripcion { get; set; }
+        public string? Observacion { get; }
+    }
+}
