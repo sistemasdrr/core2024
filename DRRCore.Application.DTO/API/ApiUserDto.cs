d2c1e665ffcf60565e3cd9cf0b71059b9e6309eb
Usuario <Usuario@LAPTOP-2JMIOR9E>
2023-08-17 10:53:52 -0500

    cambios

--- DRRCore.Application.DTO/API/ApiUserDto.cs
+++ DRRCore.Application.DTO/API/ApiUserDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace DRRCore.Application.DTO.API
 {
-    public class ApiUserDataDto
+    public class ApiUserDto
     {
         public string? CodigoAbonado { get; set; }
         public string? Environment { get; set;}
