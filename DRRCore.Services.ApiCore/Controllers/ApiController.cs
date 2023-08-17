4f9df5caff29c32f712b4c20217c14e8ebcc2569
Usuario <Usuario@LAPTOP-2JMIOR9E>
2023-08-17 10:49:59 -0500

    Prueba d Push

---
+++ DRRCore.Services.ApiCore/Controllers/ApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DRRCore.Services.ApiCore.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
