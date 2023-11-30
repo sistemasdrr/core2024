204e5cd4f85b54e9a52fb749cf931248f11d3e5d
sistemasdrr <141160460+sistemasdrr@users.noreply.github.com>
2023-11-30 09:40:55 -0500

    p

---
+++ DRRCore.Domain.Main/ApiHistoryDomain.cs
@@ -0,0 +1,55 @@
+﻿using DRRCore.Domain.Entities.SQLContext;
+using DRRCore.Domain.Interfaces;
+using DRRCore.Infraestructure.Interfaces.Repository;
+
+namespace DRRCore.Domain.Main
+{
+    public class ApiHistoryDomain : IApiHistoryDomain
+    {
+        public readonly IApiHistoryRepository _apiHistoryRepository;
+        public ApiHistoryDomain(IApiHistoryRepository apiHistoryRepository)
+        {
+            _apiHistoryRepository = apiHistoryRepository;
+        }
+
+        public async Task<bool> AddApiHistoryAsync(ApiHistory obj)
+        {
+            return await _apiHistoryRepository.AddApiHistoryAsync(obj);
+        }
+
+        public async Task<bool> DeleteApiHistoryAsync(int idApiHistory)
+        {
+            return await _apiHistoryRepository.DeleteApiHistoryAsync(idApiHistory);
+        }
+
+        public async Task<List<ApiHistory>> GetAllApiHistoryAsync()
+        {
+            return await _apiHistoryRepository.GetAllApiHistoryAsync();
+        }
+
+        public async Task<List<ApiHistory>> GetAllApiHistoryByApiUserAsync(int idApiUser)
+        {
+            return await _apiHistoryRepository.GetAllApiHistoryByApiUserAsync(idApiUser);
+        }
+
+        public async Task<List<ApiHistory>> GetAllApiHistoryBySearchAsync(string search)
+        {
+            return await _apiHistoryRepository.GetAllApiHistoryBySearchAsync(search);
+        }
+
+        public async Task<List<ApiHistory>> GetAllApiHistoryBySuccessAsync(bool success)
+        {
+            return await _apiHistoryRepository.GetAllApiHistoryBySuccessAsync(success);
+        }
+
+        public async Task<ApiHistory> GetApiHistoryByIdAsync(int idApiHistory)
+        {
+            return await _apiHistoryRepository.GetApiHistoryByIdAsync(idApiHistory);
+        }
+
+        public async Task<bool> UpdateApiHistoryAsync(ApiHistory obj)
+        {
+            return await _apiHistoryRepository.UpdateApiHistoryAsync(obj);
+        }
+    }
+}
---
+++ DRRCore.Infraestructure.Repository/SQLRepository/ApiHistoryRepository.cs
@@ -0,0 +1,116 @@
+﻿using DRRCore.Domain.Entities.SQLContext;
+using DRRCore.Infraestructure.Interfaces.Repository;
+using DRRCore.Transversal.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace DRRCore.Infraestructure.Repository.SQLRepository
+{
+    public class ApiHistoryRepository : IApiHistoryRepository
+    {
+        public async Task<bool> AddApiHistoryAsync(ApiHistory obj)
+        {
+            try
+            {
+                using var context = new SqlContext();
+                context.ApiHistories.Add(obj);
+                await context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Task<bool> DeleteApiHistoryAsync(int idApiHistory)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ApiHistory>> GetAllApiHistoryAsync()
+        {
+            try
+            {
+                using var context = new SqlContext();
+                var apiHistories = await context.ApiHistories.ToListAsync();
+                return apiHistories;
+            }catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<List<ApiHistory>> GetAllApiHistoryByApiUserAsync(int idApiUser)
+        {
+            try
+            {
+                using var context = new SqlContext();
+                var apiHistories = await context.ApiHistories.Where(x => x.IdApiUser == idApiUser).ToListAsync();
+                return apiHistories;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<List<ApiHistory>> GetAllApiHistoryBySearchAsync(string search)
+        {
+            try
+            {
+                using var context = new SqlContext();
+                var apiHistories = await context.ApiHistories.Where(x => x.Search == search).ToListAsync();
+                return apiHistories;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<List<ApiHistory>> GetAllApiHistoryBySuccessAsync(bool success)
+        {
+            try
+            {
+                using var context = new SqlContext();
+                var apiHistories = await context.ApiHistories.Where(x => x.Success == true).ToListAsync();
+                return apiHistories;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<ApiHistory> GetApiHistoryByIdAsync(int idApiHistory)
+        {
+            try
+            {
+                using var context = new SqlContext();
+                var apiHistories = await context.ApiHistories.FindAsync(idApiHistory);
+                return apiHistories;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<bool> UpdateApiHistoryAsync(ApiHistory obj)
+        {
+            try
+            {
+                using var context = new SqlContext();
+                context.ApiHistories.Update(obj);
+                await context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
