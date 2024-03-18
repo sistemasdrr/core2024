9df2a952697216a53fba48ef6eee94646c8d76e1
sistemasdrr <141160460+sistemasdrr@users.noreply.github.com>
2024-03-18 15:55:21 -0500

    p

---
+++ DRRCore.Application.DTO/Core/Request/AddOrUpdateTicketFilesRequestDto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace DRRCore.Application.DTO.Core.Request
+{
+    public class AddOrUpdateTicketFilesRequestDto
+    {
+        public int Id { get; set; }
+
+        public int? IdTicket { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Path { get; set; } = null!;
+
+        public string Extension { get; set; } = null!;
+    }
+}
---
+++ DRRCore.Domain.Interfaces/CoreDomain/ITicketFilesDomain.cs
@@ -0,0 +1,9 @@
+﻿using DRRCore.Domain.Entities.SqlCoreContext;
+
+namespace DRRCore.Domain.Interfaces.CoreDomain
+{
+    public interface ITicketFilesDomain : IBaseDomain<TicketFile>
+    {
+        Task<List<TicketFile>> GetFilesByIdTicket(int idTicket);
+    }
+}
